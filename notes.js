370a766f602d2a34ee97888db1d59160db53db56
John Fraboni <johnfraboni@hotmail.com>
2015-08-07 21:51:00 +0000

    * update show api

--- notes.js
+++ notes.js
@@ -3,22 +3,25 @@ $(function () {
     window.opspark.notes = window.opspark.notes || {};
     var notes = window.opspark.notes;
     
-    notes.show = function () {
+    notes.show = function (prependTo, file) {
+        prependTo = prependTo ? prependTo : '#container';
+        file = file ? file : '.notes/directions.html';
         if (window.location.href.match(/c9\.io|github_io|localhost|127\.0\.0\.1|file\:\/\/\//)) {
             $('<a/>')
                 .attr('id', 'btnDirections')
-                .attr('href', '.notes/directions.html')
+                .attr('href', file)
                 .attr('target', '_blank')
                 .attr('type', 'button')
                 .addClass('btn btn-primary btn-sm btn-block')
                 .css('margin-top', '5px')
                 .css('margin-bottom', '5px')
                 .html('Directions...')
-                .prependTo($('#container'));
+                .prependTo($(prependTo));
         }
     };
     
-    notes.hide = function () {
-        $('#btnDirections').detach();
+    notes.hide = function (btnId) {
+        btnId = btnId ? btnId : '#btnDirections';
+        $(btnId).detach();
     };
 });
